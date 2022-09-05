d3c4e46a700585d3d8b086727ab0444915c92c07
“kieuvu” <kieuvuvuvu@gmail.com>
2022-09-05 13:19:39 +0700

    use lombok && auto generate timestamp

--- src/main/java/com/vukm/demo/models/User.java
+++ src/main/java/com/vukm/demo/models/User.java
@@ -1,43 +1,35 @@
 package com.vukm.demo.models;
 
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * @author kieuvu
  * Created on 02/09/2022.
  */
 
+@Data
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String email;
+
     private String password;
 
-    public Long getId() {
-        return id;
-    }
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
