5c32c06b92f5ab1f3ea6219a945df2f89ef1fa0d
“kieuvu” <kieuvuvuvu@gmail.com>
2022-09-05 12:50:49 +0700

    Spring Security without the WebSecurityConfigurerAdapter

--- src/main/java/com/vukm/demo/configs/UserSecurityConfiguration.java
+++ src/main/java/com/vukm/demo/configs/UserSecurityConfiguration.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author kieuvu
@@ -14,15 +14,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class UserSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class UserSecurityConfiguration  {
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/api/register").permitAll();
+        return http.build();
     }
 }
